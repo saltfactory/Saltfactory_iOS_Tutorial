4b61804aa0591811bccd7e59f6e8e70029aceac9
saltfactory <saltfactory@gmail.com>
2012-07-29 10:04:22 +0900

    sync

---
+++ Projects/SFUITableViewTutorial/SFUITableViewTutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SFUITableViewTutorial
+//
+//  Created by SungKwang Song on 7/28/12.
+//  Copyright (c) 2012 SungKwang Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Projects/SFUITableViewTutorial/SFUITableViewTutorialTests/SFUITableViewTutorialTests.h
@@ -0,0 +1,13 @@
+//
+//  SFUITableViewTutorialTests.h
+//  SFUITableViewTutorialTests
+//
+//  Created by SungKwang Song on 7/28/12.
+//  Copyright (c) 2012 SungKwang Song. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SFUITableViewTutorialTests : SenTestCase
+
+@end
